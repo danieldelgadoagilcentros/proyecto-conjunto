865e2f18d46020a6bfb0b206c7c7c7d97a2cba4c
azash666 <ddelgadogomis@hotmail.com>
2021-09-17 10:17:58 +0200

    Ejercicio

---
+++ ejercicio.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+	const int NUM_ALUMNOS = 25;
+	char alumnos[25][21] = {
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"11",
+		"12",
+		"13",
+		"14",
+		"15",
+		"16",
+		"17",
+		"18",
+		"19",
+		"20",
+		"21",
+		"22",
+		"23",
+		"24",
+		"25"
+	};
+	
+	srand(time(NULL));
+	int pos =rand()%NUM_ALUMNOS;
+	printf("El ganador es %s\n", alumnos[pos]);
+}
